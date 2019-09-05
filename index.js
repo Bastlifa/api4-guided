7c58a99d30ed3c8be890be4a5ed39f4f1d67f35f
Eli Sacks <bastlifa@gmail.com>
2019-09-05 14:10:27 -0400

    eh?2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 const server = require('./api/server.js');
 const defaults = require('./config/defaults.js')
 
-server.listen(defaults.port, () => {
-  console.log(`\n*** Server Running on http://localhost:${defaults.port} ***\n`);
+server.listen(process.env.PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${process.env.PORT} ***\n`);
 });
